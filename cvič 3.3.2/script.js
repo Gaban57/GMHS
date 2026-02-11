dac490a3269ebc47040c67c8cddc9c8a139dc2b8
Gaban57 <gabatuma@seznam.cz>
2026-02-11 11:52:19 +0100

    čííí

---
+++ cvič 3.3.2/script.js
@@ -0,0 +1,15 @@
+let username = "Jan";
+console.log(username)
+
+username = "Petra";
+console.log(username) 
+
+let age = 21;
+console.log(age)
+let age2 = 25;
+console.log(age2)
+
+console.log(userAge1)
+console.log(userAge)
+let text = "Text"
+console.log(typeof text)
