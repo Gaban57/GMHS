c3621d331d5a98c28022468c583b0c5c5408e379
Gaban57 <gabatuma@seznam.cz>
2026-04-01 11:18:14 +0200

    Update

--- cvič 3.3.2/script.js
+++ cvič 3.3.2/script.js
@@ -6,10 +6,13 @@ console.log(username)
 
 let age = 21;
 console.log(age)
+
 let age2 = 25;
 console.log(age2)
 
-console.log(userAge1)
-console.log(userAge)
-let text = "Text"
-console.log(typeof text)
+let combinedAge = age + age2;
+console.log(combinedAge)
+
+console.log("Součet věku " + "Jan" + " a " + "Petra" + " je " + combinedAge)
+
+
