18cb96d13054ee4641441f1f719874759fcf1546
Gaban57 <gabatuma@seznam.cz>
2026-05-20 11:33:52 +0200

    Update

---
+++ 3.3.9/script.js
@@ -0,0 +1,23 @@
+console.log("It's a nice day")
+console.log('Moje jméno je "Gaban"')
+
+let username = "Tomáš";
+let message = `Moje jméno je ${username}`;
+console.log(message);
+
+let a = 5;
+let b = 10;
+console.log(`Součet je ${a + b}`);
+
+function pozdrav(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(`${pozdrav('Gaban')}`);
+
+let popis = 
+`Toto je delší text,
+který se rozprostírá
+přes několik řádků.`
+
+console.log(popis)
