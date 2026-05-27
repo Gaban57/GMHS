058ccf6ebcb6c16f756603fb53295b2751798567
Gaban57 <gabatuma@seznam.cz>
2026-05-27 11:32:36 +0200

    Update

---
+++ 3.3.10/script.js
@@ -0,0 +1,11 @@
+let myString = "Hello world!";
+let myBadString = "Hello World!"
+
+console.log(myString.length);
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.charAt(1));
+console.log(myString.indexOf("l"));
+console.log(myString.replace("H", "P"));
+console.log(myString.replaceAll("Hello world","Palword"))
+console.log(myBadString.trim());
